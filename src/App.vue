6d1010f0890c407e476f610a4cde8d8c61f2d296
Mike Slater <slatron@gmail.com>
2020-04-07 21:40:31 -0500

    split off label component

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,52 @@
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
+
+.big-label-area label {
+  font-size: 48px;
+  font-weight: bold;
+  display: block;
+  height: 100px;
+  text-align: left;
+  border-bottom: 4px solid black;
+  border-left: 4px solid black;
+  border-right: 4px solid black;
+  border-top: 4px solid black;
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 5px 15px;
+  display: flex;
+  align-items: center;
+  background-color: aqua;
+
+  svg {
+    padding-left: 15px;
+    display: none;
+    color: #b71212;
+  }
+
+  &.approved {
+    background-color: pink;
+  }
+
+  &.done {
+    background-color: lime;
+
+    svg {
+      display: block;
+    }
+
+    .but-save {
+      display: block;
+    }
+  }
+
+  input {
+    display: none;
+  }
+}
+
+
 // #nav {
 //   padding: 30px;
 //   a {
---
+++ src/components/BigLabel.vue
@@ -0,0 +1,20 @@
+<template>
+  <label
+    :class="{'done': formData[formKey]}"
+    :for="'cb-' + formKey">
+    {{formTitle}}
+    <font-awesome-icon icon="laugh-wink" />
+    <input type="checkbox" :name="'cb-' + formKey" :id="'cb-' + formKey" v-model="formData[formKey]">
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'BigLabel',
+  props: {
+    formData: { 'type': Object, 'default': {} },
+    formKey: { 'type': String, 'default': '' },
+    formTitle: { 'type': String, 'default': ''}
+  }
+}
+</script>
